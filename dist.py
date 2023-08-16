89745605acf66f5406c2362d9be3a11c3f342aa8
Raymond Li <hi@raymond.li>
2023-08-16 16:43:39 +0200

    New dist method

--- dist.py
+++ dist.py
@@ -4,15 +4,23 @@
 
 import os
 import sys
+import argparse
 
 # Error and exit on Python version < 3.11
 if sys.version_info < (3, 11):
     print("Python 3.11 or higher is required")
     sys.exit(1)
 
+parser = argparse.ArgumentParser(description="Build Nexus")
+parser.add_argument('-n', "--no-build", action="store_true", help="Skip building the executable, only setup")
+parser.add_argument('-d', "--devel", action="store_true", help="Install module locally and git hooks")
+parser.add_argument('-u', "--ui-only", action="store_true", help="Only convert ui files")
+parser.add_argument("--venv-path", type=str, help="[Relative] path to virtual environment to use")
+args = parser.parse_args()
+
 # Get OS
 os_name = "notwin"
-venv_path = "venv/bin/"
+venv_path = f"{args.venv_path if args.venv_path else 'venv'}/bin/"
 python_name = venv_path + "python"
 if sys.platform.startswith("win"):
     os_name = "win"
@@ -22,16 +30,17 @@ elif sys.platform.startswith("darwin"):
     os_name = "darwin"
 print(f"OS detected as {os_name}")
 
-# Create virtual environment if it doesn't exist
-if not os.path.isdir("venv"):
-    print("Existing virtual environment not found, creating new one...")
-    os.system("python -m venv venv")
-else:
-    print("Found existing virtual environment")
+if not args.ui_only:
+    # Create virtual environment if it doesn't exist
+    if not os.path.isdir("venv"):
+        print("Existing virtual environment not found, creating new one...")
+        os.system("python -m venv venv")
+    else:
+        print("Found existing virtual environment")
 
-# Install requirements
-print("Installing requirements...")
-os.system(f"{python_name} -m pip install --upgrade pip -r requirements.txt -r test-requirements.txt")
+    # Install requirements
+    print("Installing requirements...")
+    os.system(f"{python_name} -m pip install --upgrade pip -r requirements.txt")
 
 # Convert ui files to python
 print("Converting ui files to python...")
@@ -40,28 +49,38 @@ os.system(f"{venv_path}pyside6-uic ui/banlist.ui -o src/nexus/ui/BanlistDialog.p
 os.system(f"{venv_path}pyside6-uic ui/banword.ui -o src/nexus/ui/BanwordDialog.py")
 os.system(f"{venv_path}pyside6-uic ui/confirm.ui -o src/nexus/ui/ConfirmDialog.py")
 
-# Pyinstaller command
-build_cmd = "pyinstaller -Fn nexus src/nexus/__main__.py"
-if os_name == "notwin":
-    build_cmd += " --hidden-import pynput.keyboard._xorg --hidden-import pynput.mouse._xorg"
+if not (args.no_build or args.ui_only):
+    # Pyinstaller command
+    build_cmd = "pyinstaller -Fn nexus src/nexus/__main__.py"
+    if os_name == "notwin":
+        build_cmd += " --hidden-import pynput.keyboard._xorg --hidden-import pynput.mouse._xorg"
 
-# Build executable
-print("Building executable...")
-os.system(f"{venv_path}{build_cmd}")
+    # Build executable
+    print("Building executable...")
+    os.system(f"{venv_path}{build_cmd}")
 
-# Rename darwin executable
-if os_name == "darwin":
-    os.rename("dist/nexus", "dist/nexus-macos")
+    # Rename darwin executable
+    if os_name == "darwin":
+        os.rename("dist/nexus", "dist/nexus-macos")
 
-# Copy README and LICENSE to dist
-if os_name == "win":
-    os.system("copy README.md dist")
-    os.system("copy LICENSE dist")
-else:
-    os.system("cp README.md LICENSE dist")
+    # Copy README and LICENSE to dist
+    if os_name == "win":
+        os.system("copy README.md dist")
+        os.system("copy LICENSE dist")
+    else:
+        os.system("cp README.md LICENSE dist")
 
-print("Done! Built executable is in dist/")
+if args.devel:
+    # Install dev/test requirements
+    print("Installing dev/test requirements...")
+    os.system(f"{python_name} -m pip install --upgrade pip -r test-requirements.txt")
 
-# Setup git hooks
-print("Setting up git hooks...")
-os.system(f"{venv_path}pre-commit install")
+    # Setup git hooks
+    print("Setting up git hooks...")
+    os.system(f"{venv_path}pre-commit install")
+
+    # Install module locally
+    print("Installing module locally...")
+    os.system(f"{python_name} -m pip install -e .")
+
+print(f"Done!{' Built executable is in dist/' if not (args.no_build or args.ui_only) else ''}")
